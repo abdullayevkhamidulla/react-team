e5cb7cf54eef68886b6c5a9ad49975400afe2c05
MrFalcon1 <mashhuraboyhonova@gmail.com>
2025-12-18 16:52:45 +0500

    about page updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,6 +3,9 @@ import React from 'react'
 const About = () => {
   return (
     <div>
+      <h1>About Us</h1>
+      <p>Welcome to our application! We are dedicated to providing the best service possible.</p>
+      <p>Our team is passionate about creating innovative solutions to meet your needs.</p>
       
     </div>
   )
